7e60f8eda44b8a1add0b97b61bbb63efbce04559
Lucas Barros <lucasbarrosprofessional@gmail.com>
2026-07-31 22:41:12 -0300

    Interface and object examples

--- app.ts
+++ app.ts
@@ -1,5 +1,29 @@
-const soma = (a: number, b: number) => {
-    console.log(a+b);
+interface Pessoa {
+    nome: string,
+    idade: number,
+    profissao?: string
 }
 
-soma(2,'a');
+const pessoa: Pessoa = {
+    nome: 'Lucas',
+    idade: 26
+}
+
+const outraPessoa: Pessoa = {
+    nome: 'Roger',
+    idade: 33,
+    profissao: 'Esteticista'
+}
+
+const arrayPessoa: Array<Pessoa> = [
+    pessoa,
+    outraPessoa
+]
+
+const arrayNum: number[] = [
+    1,2,3
+]
+
+const arrayString: Array<string> = [
+    'abacaxi', 'manga', 'maçã'
+]
